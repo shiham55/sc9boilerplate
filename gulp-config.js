f79d82ad83f58735118f467408913b0bdbbc6615
Shiham M <shiham.mohamed@sg.ey.com>
2020-06-03 12:44:33 +0800

    update to config and removed files

---
+++ gulp-config.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    var config = {
+        webRoot: "C:\\Websites\\scboilerplate.local\\Website",
+        devRoot: "C:\\Projects\\AzureDevOps\\sitecore-9-helix-boilerplate",
+        solutionName: "scboilerplate",
+        buildConfiguration: "Release",
+        MSBuildToolsVersion: 'auto'
+    }
+    return config;
+}
