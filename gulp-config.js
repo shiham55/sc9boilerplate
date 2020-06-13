f965639f1031fb39ff140a7c5ab1b3f02a469ad9
Shiham M <shiham.mohamed@sg.ey.com>
2020-06-13 17:52:40 +0800

    serialization fixes

--- gulp-config.js
+++ gulp-config.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     var config = {
         webRoot: "C:\\Websites\\sc9helix.local\\Website",
-        devRoot: "C:\\Projects\\sc9boilerplate",
+        devRoot: "C:\\Projects\\sc9boilerplate\\src",
         solutionName: "scboilerplate",
         buildConfiguration: "Release",
         MSBuildToolsVersion: 'auto'
